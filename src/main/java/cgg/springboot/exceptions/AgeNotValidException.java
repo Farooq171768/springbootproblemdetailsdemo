ce0b0a0b153f3b8af7d44d61a498b18b01c0e3d2
Farooq <fmd894717@gmail.com>
2023-09-03 09:42:22 +0530

    commit 02-08-2023

---
+++ src/main/java/cgg/springboot/exceptions/AgeNotValidException.java
@@ -0,0 +1,13 @@
+package cgg.springboot.exceptions;
+
+
+public class AgeNotValidException extends RuntimeException{
+    
+    public AgeNotValidException(){
+        super();
+    }
+
+    public AgeNotValidException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/cgg/springboot/problemdetails/services/VoteService.java
@@ -0,0 +1,14 @@
+package cgg.springboot.problemdetails.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VoteService {
+    
+    public void vote(int age){
+        System.out.println("voted...");
+        //Database logic...
+        
+    }
+    
+}
